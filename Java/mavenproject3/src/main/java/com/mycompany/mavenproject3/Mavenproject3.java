25654d5ab034e698f43557042c8a37c6bcd383e6
Imalsha <140887879+Imalsha28@users.noreply.github.com>
2023-10-23 19:42:01 +0530

    Add files via upload

---
+++ Java/mavenproject3/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author AKARSHA
+ */
+public class Mavenproject3 {
+
+   String name;
+    Mavenproject3(){
+        name="Bussara";
+        System.out.println("Napuru "+name);
+    }
+     public static void main(String[]arg){
+         Mavenproject3 obj =new Mavenproject3();
+     }
+
+}
